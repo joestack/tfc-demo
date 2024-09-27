28534619e2c1a9a7f3a692e149d025d9920c55f5
JoeStack <joern@hashicorp.com>
2024-09-27 12:40:33 +0200

    pw 41

--- main.tf
+++ main.tf
@@ -165,7 +165,7 @@ resource "aws_instance" "windows" {
     # Be sure to set the username and password on these two lines. Of course this is not a good
     # security practice to include a password at command line.
     # PW as variable seems not to work TODO: sitch to TEMPLATE
-    $User = "${var.windows_username}"
+    $User = "$${var.windows_username}"
     $Password = ConvertTo-SecureString "5up3r53cur321" -AsPlainText -Force
     New-LocalUser $User -Password $Password
     Add-LocalGroupMember -Group "Remote Desktop Users" -Member $User
