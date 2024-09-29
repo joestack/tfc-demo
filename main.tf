8f1af0ab4d2f79d87fa9fa5f6ea2a1ce9b1287ea
JoeStack <joern@hashicorp.com>
2024-09-29 14:30:11 +0200

    other pw

--- main.tf
+++ main.tf
@@ -164,9 +164,8 @@ resource "aws_instance" "windows" {
     <powershell>
     # Be sure to set the username and password on these two lines. Of course this is not a good
     # security practice to include a password at command line.
-    # PW as variable seems not to work TODO: switch to TEMPLATES
     $User = "RdpUser"
-    $Password = ConvertTo-SecureString "5up3r53cur321" -AsPlainText -Force
+    $Password = ConvertTo-SecureString "S3curePa55w0rd" -AsPlainText -Force
     New-LocalUser $User -Password $Password
     Add-LocalGroupMember -Group "Remote Desktop Users" -Member $User
     Add-LocalGroupMember -Group "Administrators" -Member $User
